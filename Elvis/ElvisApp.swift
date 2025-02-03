9bf8ea7be591147e7761ffaf262d0d4e7361f4c8
Robert <robert@krahe.one>
2025-02-03 20:02:32 +0000

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Robert Krahe on 03.02.25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
